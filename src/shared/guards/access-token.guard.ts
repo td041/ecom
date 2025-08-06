3c9658cc0d2a985e3b24456a4f2b3c7277b7652b
TrinhDuc <trinhtrantrungduc@gmail.com>
2025-08-06 23:23:21 +0700

    authorization: check permissions on requests

--- src/shared/guards/access-token.guard.ts
+++ src/shared/guards/access-token.guard.ts
@@ -1,24 +1,69 @@
-import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException, ForbiddenException } from '@nestjs/common'
+import { Request } from 'express'
 import { REQUEST_USER_KEY } from 'src/shared/constants/auth.constants'
+import { HTTPMethod } from 'src/shared/constants/role.constants'
+import { PrismaService } from 'src/shared/services/prisma.service'
 import { TokenService } from 'src/shared/services/token.service'
+import { AccessTokenPayload } from 'src/shared/types/jwt.type'
 
 @Injectable()
 export class AccessTokenGuard implements CanActivate {
-  constructor(private readonly TokenService: TokenService) {}
+  constructor(
+    private readonly TokenService: TokenService,
+    private readonly prismaService: PrismaService,
+  ) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest()
-    const accessToken = request.headers['authorization']?.split(' ')[1] // Assuming Bearer token format
-    // console.log('accessToken', accessToken)
-    if (!accessToken) {
-      throw new UnauthorizedException()
-    }
+    const decodedAccessToken = await this.extractAndValidateToken(request)
+    await this.validateUserPermission(decodedAccessToken, request)
+    return true
+  }
+  private async extractAndValidateToken(request: Request): Promise<AccessTokenPayload> {
+    const accessToken = this.extractAccessTokenFromHeader(request)
     try {
       const decodedAccessToken = await this.TokenService.verifyAccessToken(accessToken)
       request[REQUEST_USER_KEY] = decodedAccessToken // gán key đông vào request
-      return true
-    } catch (error) {
-      console.log(error)
-        throw new UnauthorizedException()
+      return decodedAccessToken
+    } catch {
+      throw new UnauthorizedException('Error.InvalidAccessToken')
+    }
+  }
+  private extractAccessTokenFromHeader(request: Request): string {
+    const accessToken = request.headers.authorization?.split(' ')[1]
+    if (!accessToken) {
+      throw new UnauthorizedException('Error.MissingAccessToken')
+    }
+    return accessToken
+  }
+  private async validateUserPermission(decodedAccessToken: AccessTokenPayload, request: Request): Promise<void> {
+    const roleId: number = decodedAccessToken.roleId
+    const path: string = request.route.path
+    const method = request.method as keyof typeof HTTPMethod
+    const role = await this.prismaService.role
+      .findUniqueOrThrow({
+        where: {
+          id: roleId,
+          deletedAt: null,
+        },
+        include: {
+          permissions: {
+            where: {
+              deletedAt: null,
+              path,
+              method,
+            },
+          },
+        },
+      })
+      .catch(() => {
+        throw new ForbiddenException()
+      })
+    console.log(role.permissions)
+    // const canAccess = role.permissions.some((permission) => permission.path === path && permission.method === method)
+    const canAccess = role.permissions.length > 0
+    console.log(canAccess)
+    if (!canAccess) {
+      throw new ForbiddenException()
     }
   }
 }
