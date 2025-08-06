e26ca01bddb209e542d6c34420b9eae5eb467289
TrinhDuc <trinhtrantrungduc@gmail.com>
2025-08-06 19:19:15 +0700

    chore: change variable name in permissionService

--- src/routes/permission/permission.service.ts
+++ src/routes/permission/permission.service.ts
@@ -19,11 +19,11 @@ export class PermissionService {
   }
 
   async findById(id: number) {
-    const Permission = await this.permissionRepo.findById(id)
-    if (!Permission) {
+    const permission = await this.permissionRepo.findById(id)
+    if (!permission) {
       throw NotFoundRecordException
     }
-    return Permission
+    return permission
   }
 
   async create({ body, createdById }: { body: CreatePermissionBodyType; createdById: number }) {
