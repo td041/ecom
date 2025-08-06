d86c27be46a1b2a7530656f21a550a7a9d2722f9
TrinhDuc <trinhtrantrungduc@gmail.com>
2025-08-06 17:03:23 +0700

    config: config model, dto and error for role schema

---
+++ src/routes/role/role.model.ts
@@ -0,0 +1,57 @@
+import { RoleSchema } from 'src/routes/auth/auth.model'
+import { PermissionSchema } from 'src/routes/permission/permission.model'
+import { z } from 'zod'
+
+export const RoleWithPermissionsSchema = RoleSchema.extend({
+  permissions: z.array(PermissionSchema),
+})
+
+export const GetRolesResSchema = z.object({
+  data: z.array(RoleSchema),
+  totalItems: z.number(),
+  page: z.number(),
+  limit: z.number(),
+  totalPages: z.number(),
+})
+
+export const GetRolesQuerySchema = z
+  .object({
+    page: z.coerce.number().int().positive().default(1),
+    limit: z.coerce.number().int().positive().default(10),
+  })
+  .strict()
+
+export const GetRoleParamsSchema = z
+  .object({
+    roleId: z.coerce.number(),
+  })
+  .strict()
+
+export const GetRoleDetailResSchema = RoleWithPermissionsSchema
+
+export const CreateRoleBodySchema = RoleSchema.pick({
+  name: true,
+  description: true,
+  isActive: true,
+}).strict()
+
+export const CreateRoleResSchema = RoleSchema
+
+export const UpdateRoleBodySchema = RoleSchema.pick({
+  name: true,
+  description: true,
+  isActive: true,
+})
+  .extend({
+    permissionIds: z.array(z.number()),
+  })
+  .strict()
+
+export type RoleWithPermissionsType = z.infer<typeof RoleWithPermissionsSchema>
+export type GetRolesResType = z.infer<typeof GetRolesResSchema>
+export type GetRolesQueryType = z.infer<typeof GetRolesQuerySchema>
+export type GetRoleDetailResType = z.infer<typeof GetRoleDetailResSchema>
+export type CreateRoleResType = z.infer<typeof CreateRoleResSchema>
+export type CreateRoleBodyType = z.infer<typeof CreateRoleBodySchema>
+export type GetRoleParamsType = z.infer<typeof GetRoleParamsSchema>
+export type UpdateRoleBodyType = z.infer<typeof UpdateRoleBodySchema>
