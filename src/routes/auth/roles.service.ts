a238f1f9b925f72ce2f3cec010d43bbfc015989c
TrinhDuc <trinhtrantrungduc@gmail.com>
2025-08-06 22:03:22 +0700

    fix: fix roles.service to register

--- src/routes/auth/roles.service.ts
+++ src/routes/auth/roles.service.ts
@@ -12,7 +12,7 @@ export class RolesService {
       return this.clientRoleId
     }
     const role: RoleType = await this.prismaService.$queryRaw`
-      SELECT * from Role WHERE name = ${RoleName.Client} AND "deletedAt" IS NULL LIMIT 1;
+      SELECT * from "Role" WHERE name = ${RoleName.Client} AND "deletedAt" IS NULL LIMIT 1;
     `.then((res: RoleType[]) => {
       if (res.length === 0) {
         throw new Error('Client role not found')
